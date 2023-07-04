9662a3f1ae33bd5363b0f601dc574e68cfbe1300
Afrizal Mufriz Fouji <afrizal.mufriz25@gmail.com>
2023-07-04 22:33:43 +0700

    first commit

---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,39 @@
+import { g, auth, config } from '@grafbase/sdk';
+
+const User = g.model('User', {
+  name: g.string().length({ min: 2, max: 20 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string(),
+  githubUrl: g.url().optional(),
+  linkedInUrl: g.url().optional(),
+  projects: g.relation(() => Project).list().optional(),
+});
+
+const Project = g
+  .model('Project', {
+    title: g.string().length({ min: 2 }),
+    description: g.string(),
+    image: g.url(),
+    liveSiteUrl: g.url(),
+    githubUrl: g.url(),
+    category: g.string().search(),
+    createdBy: g.relation(() => User),
+  })
+  .auth((rules) => {
+    rules.public().read();
+    rules.private().create().delete().update();
+  });
+
+const jwt = auth.JWT({
+  issuer: 'grafbase',
+  secret: g.env('NEXTAUTH_SECRET'),
+});
+
+export default config({
+  schema: g,
+  auth: {
+    providers: [jwt],
+    rules: (rules) => rules.private(),
+  },
+});
